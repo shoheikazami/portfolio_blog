133c4be666a1bef9dff130112d6f41d454dc0abf
Ryo Nakajima <ryo_naka2_3728@icloud.com>
2026-09-10 12:26:34 +0900

    Align API permissions with template rules

--- apiapp/tests.py
+++ apiapp/tests.py
@@ -1,4 +1,5 @@
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -6,6 +7,17 @@ from blog.models import Post
 
 
 class PostAPIPermissionTests(APITestCase):
+
+    def setUp(self):
+        user_model = get_user_model()
+        self.regular_user = user_model.objects.create_user(
+            username='regular',
+            password='test-password',
+        )
+        self.superuser = user_model.objects.create_superuser(
+            username='admin',
+            password='test-password',
+        )
 
     def test_anonymous_user_can_read_posts(self):
         response = self.client.get(reverse('post-list'))
@@ -21,3 +33,27 @@ class PostAPIPermissionTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(Post.objects.count(), 0)
+
+    def test_regular_user_cannot_create_post(self):
+        self.client.force_authenticate(user=self.regular_user)
+
+        response = self.client.post(
+            reverse('post-list'),
+            {'title': 'title', 'text': 'text'},
+            format='json',
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Post.objects.count(), 0)
+
+    def test_superuser_can_create_post(self):
+        self.client.force_authenticate(user=self.superuser)
+
+        response = self.client.post(
+            reverse('post-list'),
+            {'title': 'title', 'text': 'text'},
+            format='json',
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Post.objects.count(), 1)
--- apiapp/views.py
+++ apiapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from rest_framework import filters, permissions, viewsets
+from rest_framework import filters, viewsets
 from blog.models import Post
 from .serializers import PostSerializer
+from .permissions import IsSuperuserOrReadOnly
 
 
 class PostAPIView(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-date')
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsSuperuserOrReadOnly]
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['title', 'text']
     ordering_fields = ['date', 'title']
