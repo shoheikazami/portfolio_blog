29e1887732af049a9c0ad077678ca1874bf771c1
Ryo Nakajima <ryo_naka2_3728@icloud.com>
2026-09-10 12:23:52 +0900

    Fix Render build script

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
+set -o nounset
+set -o pipefail
 
-pip install -r requirements.txt
-
+python -m pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py superuser
